6ffae3e7eb5b7ccc79f628bcc548a59ea58e046b
rashidsarkar <rashidrock558@gmail.com>
2025-10-15 12:33:31 +0600

    `Refine email validation and remove first name capital letter check, add name length check`

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -6,9 +6,7 @@ const registerUserValidationSchema = z.object({
       .string()
       .trim()
       .min(1, 'Name is required')
-      .refine((value) => /^[A-Z]/.test(value), {
-        message: 'First Name must start with a capital letter',
-      }),
+      .max(100, 'Name is too long'),
     email: z.string().email('Invalid email format').min(1, 'Email is required'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
   }),
